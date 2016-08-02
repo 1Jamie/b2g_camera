87a0b4b2821a1962c1513f243f0784f4878ce7c5
Jamie1 <charja13@gmail.com>
2016-08-02 15:30:16 -0400

    quick updates for adjustment for basic display, also changing my base dir for it

--- scripts/app.js
+++ scripts/app.js
@@ -1,76 +1,16 @@
-//check to make sure that getusermedia is supported
-function hasGetUserMedia() {
-  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
+(function() {
+  // The width and height of the captured photo.
 
-if (hasGetUserMedia()) {
-  // Good to go!
-} else {
-  alert('getUserMedia() is not supported in your browser');
-}
-//initialzation
-function() {
-	var width = 320;  //scale photo
-	var height = 0;
+  var width = 380;    // need to scale this properly for device
+  var height = 0;    
 
-	var streaming = false;
-	var video = null;
-	var canvas = null;
-	var photo = null;
-	var startButton = null
-}
-//does the obvious starts the dang thing
-function startup () {
-	video = document.getElementById('video');
-	canvas = doccument.getElementById('canvas');
-	photo = doccument.getElementById('photo');
-	startButton = doccument.getElementById('startButton')
-}
-navigator.getMedia = (navigator.getusermedia ||
-						navigator.webkitGetUserMedia ||
-						navigator.mozGetUserMedia||
-						navigator.msGetUserMedia);
+// tell us whether were straming form the camera currently or not, we havent started yet so... no
 
-navigator.getMedia(
-{
-	video: true;
-	audio: false;
-},
-	function(stream) {
-		if (navigator.mozGetUserMedia) {
-			video.mozSrcObject = stream;
-		} else {
-			var vendorURL= window.URL || window.webkitURL ;
-			video.src - vendorURL.createObjectURL(stream) ;
-		}
-		video.play();
-		}
-		function(err) {
-			console.log("there is an issue somewhere!" + err);
-		}
-);
+  var streaming = false;
 
-//geting feed for picture running
-video.addEventListener('canplay', function(ev){
-	if (!streaming) {
-		height = video.videoHeight / (video.videoWidth/width);
+  // does the obvious... obviously clears the photo
 
-		if (isNaN(height)) {
-			height = width / (4/3);
-		}
-		video.setAttribute('width', width);
-		video.setAttribute('height', height);
-		canvas.setAttribute('width', width);
-		canvas.setAttribute('height', height);
-	}	
-}, false);
-startButton.addEventListener('click', function(ev){
-	takepicture();
-	ev.preventDefault();
-}, false);
-//this function clears the photo box
-function clearphoto() {
+  function clearPic() {
     var context = canvas.getContext('2d');
     context.fillStyle = "#AAA";
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -78,19 +18,97 @@ function clearphoto() {
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
   }
-clearphoto();
-// this fucntion does 
-function takepicture() {
-	var context = canvas.getContext('2d');
-	if (width && height) {
-		canvas.width = width;
-		canvas.height = height;
-		context.drawImage(video, 0, 0, width, height);
 
-		var data = canvas.toDateURL('image/png');
-		photo.setAttribute('src', data);
+//initalizing the elements properties we need and assigning to the specific elements
 
-	} else {
-		clearphoto()
-	}
-}
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+
+  console.log("variables set moving onto startup");
+
+  function startup() {
+    video = document.getElementById('lFeed');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    startbutton = document.getElementById('capture');
+
+    console.log("variables attaced to elements moving onto setting up get user media");
+
+    navigator.getMedia = ( navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia);
+
+    navigator.getMedia(
+      {
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+          console.log('stream should be getting ready, you are on moz');
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+          console.log('stream should be ready now, you are on non-moz');
+        }
+        video.play();
+        console.log("feed should be displayed now, if not check for issues");
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );
+
+    video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
+      
+      
+        if (isNaN(height)) {
+          height = width / (4/3);
+        }
+      
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    console.log('height and width of photo taken should now be set if not before');
+
+    startbutton.addEventListener('click', function(ev){
+      takepicture();
+       console.log("picture should have been taken")
+      ev.preventDefault();
+    }, false);
+    
+    clearPic();
+  }
+
+
+//capture photo by taking a frame from the video feed and drawing it elsewhere then calling it
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+    
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+    } else {
+      clearPic();
+    }
+  }
+
+// Start everything up after load
+
+  window.addEventListener('load', startup, false);
+})();
